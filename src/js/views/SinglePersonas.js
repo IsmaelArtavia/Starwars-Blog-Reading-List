94bfe7615dc57d01e4fe5367ef1d8463c1978232
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-18 02:39:57 +0000

    css atualizacion

--- src/js/views/SinglePersonas.js
+++ src/js/views/SinglePersonas.js
@@ -9,7 +9,7 @@ import PropTypes from "prop-types";
 export const Single = props => {
 	const { store, actions } = useContext(Context);
 
-	console.log(actions);
+	//console.log(actions);
 	const params = useParams();
 
 	//console.log(params);
@@ -34,34 +34,79 @@ export const Single = props => {
 
 	return (
 		<div className="jumbotron">
-			<img
-				src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Luke_Skywalker_con_traje_de_piloto.jpg/368px-Luke_Skywalker_con_traje_de_piloto.jpg"
-				className="rounded float-end"
-				alt="..."
-			/>
-			<h5 className="display-4">{datosPersonas.name} </h5>
-			<p>
-				Nam scelerisque mi ultrices, ullamcorper elit nec, pellentesque massa. Morbi egestas nibh magna, a
-				ullamcorper sem vulputate vel. Praesent volutpat neque ac ex vehicula auctor. Aliquam pulvinar orci id
-				elit lobortis, nec consequat eros sollicitudin. Suspendisse mollis orci id nulla maximus, eu lacinia
-				ligula aliquet. In dignissim odio sit amet scelerisque egestas. Suspendisse nulla diam, hendrerit eget
-				convallis sit amet, varius nec urna. Duis eleifend sed nibh at imperdiet. Ut volutpat semper nulla, sed
-				tempus metus. Aliquam faucibus rhoncus ornare. Cras ut gravida quam.{" "}
-			</p>
+			<div className="container1">
+				<div className="col-12">
+					<img
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Luke_Skywalker_con_traje_de_piloto.jpg/368px-Luke_Skywalker_con_traje_de_piloto.jpg"
+						className="rounded float-end"
+						alt="..."
+					/>
+				</div>
+				<div className="col-6">
+					<h5 className="display-4">{datosPersonas.name} </h5>
+					<p>
+						Nam scelerisque mi ultrices, ullamcorper elit nec, pellentesque massa. Morbi egestas nibh magna,
+						a ullamcorper sem vulputate vel. Praesent volutpat neque ac ex vehicula auctor. Aliquam pulvinar
+						orci id elit lobortis, nec consequat eros sollicitudin. Suspendisse mollis orci id nulla
+						maximus, eu lacinia ligula aliquet. In dignissim odio sit amet scelerisque egestas. Suspendisse
+						nulla diam, hendrerit eget convallis sit amet, varius nec urna. Duis eleifend sed nibh at
+						imperdiet. Ut volutpat semper nulla, sed tempus metus. Aliquam faucibus rhoncus ornare. Cras ut
+						gravida quam.{" "}
+					</p>
+				</div>
+			</div>
 
 			<hr className="my-4" />
-			<h3>Name {datosPersonas.name} </h3>
-			<h3>Birth Year {datosPersonas.birth_year} </h3>
-			<h3> Gender {datosPersonas.gender} </h3>
-			<h3> Height {datosPersonas.height} </h3>
-			<h3> Skin color {datosPersonas.skin_color} </h3>
-			<h3> Eye Color {datosPersonas.eye_color} </h3>
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
+			<div className="container">
+				<div className="row align-items-start">
+					<div className="col-2">
+						<h3>
+							Name <br /> <br /> {datosPersonas.name}{" "}
+						</h3>
+					</div>
+					<div className="col-2">
+						<h3>
+							Birth <br /> Year <br />
+							<br />
+							{datosPersonas.birth_year}{" "}
+						</h3>
+					</div>
+					<div className="col-2">
+						<h3>
+							{" "}
+							Gender <br /> <br /> <br />
+							{datosPersonas.gender}{" "}
+						</h3>
+					</div>
+					<div className="col-2">
+						<h3>
+							Height <br /> <br /> <br /> {datosPersonas.height}
+						</h3>
+					</div>
+					<div className="col-2">
+						<h3>
+							{" "}
+							Skin <br /> color <br />
+							<br /> {datosPersonas.skin_color}{" "}
+						</h3>
+					</div>
+					<div className="col-2">
+						<h3>
+							{" "}
+							Eye <br /> Color <br /> <br /> {datosPersonas.eye_color}{" "}
+						</h3>
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<Link to="/">
+					<span className="btn btn-primary btn-lg" href="#" role="button">
+						Back home
+					</span>
+				</Link>
+			</div>
 		</div>
 	);
 };
--- src/js/views/singlePlanetas.js
+++ src/js/views/singlePlanetas.js
@@ -19,33 +19,66 @@ export const SinglePlanetas = props => {
 	//console.log(datosPlanetas.name);
 	return (
 		<div className="jumbotron">
-			<h5 className="display-4">{datosPlanetas.name} </h5>
-			<img
-				src="https://static.wikia.nocookie.net/starwars/images/7/72/Teth-TVE.png/revision/latest?cb=20190423045047"
-				className="rounded float-end"
-				alt="..."
-			/>
+			<div className="container1">
+				<div className="col-12">
+					<img
+						src="https://static.wikia.nocookie.net/starwars/images/7/72/Teth-TVE.png/revision/latest?cb=20190423045047"
+						className="rounded float-end"
+						alt="..."
+					/>
+				</div>
 
-			<p>
-				Nam scelerisque mi ultrices, ullamcorper elit nec, pellentesque massa. Morbi egestas nibh magna, a
-				ullamcorper sem vulputate vel. Praesent volutpat neque ac ex vehicula auctor. Aliquam pulvinar orci id
-				elit lobortis, nec consequat eros sollicitudin. Suspendisse mollis orci id nulla maximus, eu lacinia
-				ligula aliquet. In dignissim odio sit amet scelerisque egestas. Suspendisse nulla diam, hendrerit eget
-				convallis sit amet, varius nec urna. Duis eleifend sed nibh at imperdiet. Ut volutpat semper nulla, sed
-				tempus metus. Aliquam faucibus rhoncus ornare. Cras ut gravida quam.{" "}
-			</p>
+				<div className="col-6">
+					<h5 className="display-4">{datosPlanetas.name} </h5>
+					<p>
+						Nam scelerisque mi ultrices, ullamcorper elit nec, pellentesque massa. Morbi egestas nibh magna,
+						a ullamcorper sem vulputate vel. Praesent volutpat neque ac ex vehicula auctor. Aliquam pulvinar
+						orci id elit lobortis, nec consequat eros sollicitudin. Suspendisse mollis orci id nulla
+						maximus, eu lacinia ligula aliquet. In dignissim odio sit amet scelerisque egestas. Suspendisse
+						nulla diam, hendrerit eget convallis sit amet, varius nec urna. Duis eleifend sed nibh at
+						imperdiet. Ut volutpat semper nulla, sed tempus metus. Aliquam faucibus rhoncus ornare. Cras ut
+						gravida quam.{" "}
+					</p>
+				</div>
+			</div>
+
 			<hr className="my-4" />
-			<h3>Name {datosPlanetas.name} </h3>
-			<h3>Climate {datosPlanetas.climate} </h3>
-			<h3> Population {datosPlanetas.population} </h3>
-			<h3> Orbital Period {datosPlanetas.orbital_period} </h3>
-			<h3> Rotation Period {datosPlanetas.rotation_period} </h3>
-			<h3> Diameter {datosPlanetas.diameter} </h3>
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
+
+			<div className="container">
+				<div className="row align-items-start">
+					<div className="col-2">
+						<h3>Name {datosPlanetas.name} </h3>
+					</div>
+
+					<div className="col-2">
+						<h3>Climate {datosPlanetas.climate} </h3>
+					</div>
+
+					<div className="col-2">
+						<h3> Population {datosPlanetas.population} </h3>
+					</div>
+
+					<div className="col-2">
+						<h3> Orbital Period {datosPlanetas.orbital_period} </h3>
+					</div>
+
+					<div className="col-2">
+						<h3> Rotation Period {datosPlanetas.rotation_period} </h3>
+					</div>
+
+					<div className="col-2">
+						<h3> Diameter {datosPlanetas.diameter} </h3>
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<Link to="/">
+					<span className="btn btn-primary btn-lg" href="#" role="button">
+						Back home
+					</span>
+				</Link>
+			</div>
 		</div>
 	);
 };
